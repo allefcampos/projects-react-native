8df3a8f4dd3279a1a5977186d9171f2ead6c68a2
allefcampos <allef-cleiton88@hotmail.com>
2021-07-19 11:37:28 -0300

    Inicializando o projeto

---
+++ Exercicios/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { Comp1, Comp2 } from './components/Multi';
+import PrimeiroComponente from './components/Primeiro'
+
+export default () => (
+    <View>
+        <PrimeiroComponente />
+    </View>    
+)
---
+++ Exercicios/src/components/Multi.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+function Comp() {
+    return <Text>Componente Oficial</Text>
+}
+
+function Comp1() {
+    return <Text>Comp #01</Text>
+}
+
+function Comp2() {
+    return <Text>Comp #02</Text>
+}
+
+export { Comp1, Comp2}
